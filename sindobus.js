26e8e6f91e55ce4c91a0b92de77655db707eb9d3
yeonju2 <yeonju2@naver.com>
2019-05-17 11:13:50 +0900

    test3

--- sindobus.js
+++ sindobus.js
@@ -8,6 +8,33 @@
         return {status: 2, msg: 'Ready'};
     };
 
+    ext.my_first_block = function() {
+        console.log('hello');
+        // Code that gets executed when the block is run
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            // Block type, block name, function name
+            [' ', 'my first block', 'my_first_block'],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('My first extension', descriptor, ext);
+})({});
+/*
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
               //url: 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=imperial',
     ext.get_temp = function(location, callback) {
         // Make an AJAX call to the Open Weather Maps API
@@ -33,3 +60,4 @@
     // Register the extension
     ScratchExtensions.register('Weather extension', descriptor, ext);
 })({});
+*/
